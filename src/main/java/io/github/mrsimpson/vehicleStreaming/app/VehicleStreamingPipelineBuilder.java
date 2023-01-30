a7da0a43ed5ce985993d76b156213469af4231b2
Oliver Jägle <oliver.jaegle@deutschebahn.com>
2023-01-30 15:22:37 +0100

    Use builder for optional params

---
+++ src/main/java/io/github/mrsimpson/vehicleStreaming/app/VehicleStreamingPipelineBuilder.java
@@ -0,0 +1,38 @@
+package io.github.mrsimpson.vehicleStreaming.app;
+
+import io.github.mrsimpson.vehicleStreaming.util.VehicleEvent;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+
+public class VehicleStreamingPipelineBuilder {
+    private StreamExecutionEnvironment env;
+    private RichParallelSourceFunction<VehicleEvent> vehicleEvents;
+    private SinkFunction<Tuple2<String, Integer>> rentalsCountSink;
+    private SinkFunction<Tuple2<String, Integer>> returnsCountSink;
+
+    public VehicleStreamingPipelineBuilder setEnv(StreamExecutionEnvironment env) {
+        this.env = env;
+        return this;
+    }
+
+    public VehicleStreamingPipelineBuilder setVehicleEvents(RichParallelSourceFunction<VehicleEvent> vehicleEvents) {
+        this.vehicleEvents = vehicleEvents;
+        return this;
+    }
+
+    public VehicleStreamingPipelineBuilder setRentalsCountSink(SinkFunction<Tuple2<String, Integer>> rentalsCountSink) {
+        this.rentalsCountSink = rentalsCountSink;
+        return this;
+    }
+
+    public VehicleStreamingPipelineBuilder setReturnsCountSink(SinkFunction<Tuple2<String, Integer>> returnsCountSink) {
+        this.returnsCountSink = returnsCountSink;
+        return this;
+    }
+
+    public VehicleStreamingPipeline createVehicleStreamingPipeline() {
+        return new VehicleStreamingPipeline(env, vehicleEvents, rentalsCountSink, returnsCountSink);
+    }
+}
