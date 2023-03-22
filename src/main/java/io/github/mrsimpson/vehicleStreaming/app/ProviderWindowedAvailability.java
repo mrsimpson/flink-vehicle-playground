2dec884b10cd1fc3fd052a8647cef0aaa91b9782
Oliver Jägle <oliver.jaegle@deutschebahn.com>
2023-03-22 14:36:39 +0100

    Make ProviderWindowedAvailability serializable

--- src/main/java/io/github/mrsimpson/vehicleStreaming/app/ProviderWindowedAvailability.java
+++ src/main/java/io/github/mrsimpson/vehicleStreaming/app/ProviderWindowedAvailability.java
@@ -5,13 +5,13 @@ import io.github.mrsimpson.vehicleStreaming.util.FormattablePojo;
 import java.time.Instant;
 
 class ProviderWindowedAvailability implements FormattablePojo {
-    String provider;
-    Long totalAvailabilityMilliseconds;
-    Double totalAvailabilityRatio;
-    Instant startOfWindow;
-    Instant endOfWindow;
+    public String provider;
+    public Long totalAvailabilityMilliseconds;
+    public Double totalAvailabilityRatio;
+    public Instant startOfWindow;
+    public Instant endOfWindow;
 
-    public ProviderWindowedAvailability(String provider, Long totalAvailabilityMilliseconds, Double totalAvailabilityRatio, Instant startOfWindow, Instant endOfWindow){
+    public ProviderWindowedAvailability(String provider, Long totalAvailabilityMilliseconds, Double totalAvailabilityRatio, Instant startOfWindow, Instant endOfWindow) {
         this.provider = provider;
         this.totalAvailabilityMilliseconds = totalAvailabilityMilliseconds;
         this.totalAvailabilityRatio = totalAvailabilityRatio;
